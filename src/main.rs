4d6bd9e6f511d790ec284d777b3e2c5be2ec07a6
Алексей Бельченков <u608110@gmail.com>
2023-02-28 14:50:54 +0400

    Read params from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, fs};
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    
+    Config { query, filename }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Поиск {}", config.query);
+    println!("В файле {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+                                        .expect("Что-то пошло не так при чтении файла");
+
+    println!("С текстом:\n{}", contents);
 }
