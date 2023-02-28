af9b32f5b592d94bdd16e082ff6d766c3243b00a
Алексей Бельченков <u608110@gmail.com>
2023-02-28 17:35:27 +0400

    print in file

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Проблема при разборе аргументов: {}", err);
+        eprintln!("Проблема при разборе аргументов: {}", err);
         process::exit(1);
     });
 
@@ -13,7 +13,7 @@ fn main() {
     println!("В файле {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Ошибка в приложении: {}", e);
+        eprintln!("Ошибка в приложении: {}", e);
         process::exit(1);
     }
 }
